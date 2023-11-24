d67db46f0f65157a6bcf17a4c4553abe1c27fc5b
Tuong Tran <tuongtranngoc@gmail.com>
2023-11-24 15:43:35 +0700

    update:pytorch lightning

--- src/pl_modules/pl_model.py
+++ src/pl_modules/pl_model.py
@@ -12,6 +12,7 @@ from src.utils.losses import CTCLoss
 
 from . import *
 
+logger = Logger.get_logger("TRAINING")
 
 class LitCRNN(pl.LightningModule):
     def __init__(self) -> None:
@@ -22,13 +23,13 @@ class LitCRNN(pl.LightningModule):
         self.loss_func = CTCLoss()
         self.best_acc = 0.0
         self.val_metrics = BatchMeter()
+        self.train_metrics = BatchMeter()
         self.automatic_optimization = False
 
     def on_train_start(self):
         pass
 
     def training_step(self, batch, batch_idx):
-        train_metrics = BatchMeter()
         images, labels, labels_len = batch
         bz = images.size(0)
         outs = self.model(images)
@@ -36,13 +37,12 @@ class LitCRNN(pl.LightningModule):
         labels_len = torch.flatten(labels_len)
         images_len = torch.tensor([outs.size(0)] * bz, dtype=torch.long)
         loss = self.loss_func(log_probs, labels, images_len, labels_len)
-        train_metrics.update(loss)
-        self.log("loss", train_metrics.get_value("mean"), prog_bar=False)
+        self.train_metrics.update(loss)
+        self.log("Loss", self.train_metrics.get_value("mean"), prog_bar=False)
         optim = self.optimizers()
         optim.zero_grad()
         self.manual_backward(loss)
         optim.step()
-        # self.lr_schedulers().step()
 
     def validation_step(self, batch, batch_idx):
         images, labels, labels_len = batch
@@ -54,7 +54,7 @@ class LitCRNN(pl.LightningModule):
 
         log_probs = log_probs.cpu().detach().numpy()
         labels = labels.cpu().detach().numpy().tolist()
-        
+    
         preds = best_path_decode(log_probs)
         acc = compute_acc(preds, labels, labels_len, self.id2char)
         self.val_metrics.update(acc)
@@ -62,7 +62,8 @@ class LitCRNN(pl.LightningModule):
         
     def on_validation_epoch_end(self):
         current_acc = self.val_metrics.get_value("mean")
-        self.log("acc", current_acc, prog_bar=False)
+        self.log("Acc", current_acc, prog_bar=False)
+        logger.info(f"Acc: {current_acc :.3f}")
         if current_acc > self.best_acc:
             self.best_acc = current_acc
         
@@ -71,4 +72,5 @@ class LitCRNN(pl.LightningModule):
         return [optimizer]
 
     def on_train_end(self):
+        logger.info(f"Loss: {self.train_metrics.get_value('mean')}")
         self.log(f"Best acc: {self.best_acc :.3f}")
